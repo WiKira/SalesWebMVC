d60ff65f5ec41a096282cee0d11830fc96da1cf2
WiKira <william.ccgo@gmail.com>
2023-03-03 21:22:02 -0300

    Seller Controller e Index Seller

--- SalesWebMVC/Models/Seller.cs
+++ SalesWebMVC/Models/Seller.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 
 namespace SalesWebMVC.Models
@@ -9,7 +10,9 @@ namespace SalesWebMVC.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
+        [DisplayFormat(DataFormatString = "{0:d}")]
         public DateTime BirthDate { get; set; }
+        [DisplayFormat(DataFormatString = "{0:N2}")]
         public double BaseSalary { get; set; }
         public Department Department { get; set; }
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
