205ee176277a1a17dc925a0546a547fe51e48012
WiKira <william.ccgo@gmail.com>
2023-03-05 12:07:12 -0300

    Details e Eager Loading

--- SalesWebMVC/Controllers/SellersController.cs
+++ SalesWebMVC/Controllers/SellersController.cs
@@ -59,5 +59,21 @@ namespace SalesWebMVC.Controllers
             _sellerService.Remove(id);
             return RedirectToAction("Index");
         }
+
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var obj = _sellerService.FindById(id.Value);
+
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
     }
 }
